90fbf952b68575a51b50a7c39e43db8d1b1f952f
koushik global <koushikofficial@yahoo.com>
2022-01-06 17:24:35 +0000

    pytest configured

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages  
+
+
+setup(
+
+  name = "src",version='1.0',description ="its an MLOPS Project",
+  author ="koushik", author_email='koushikofficial@yahoo.com',
+  packages = find_packages(),license="MIT"
+)

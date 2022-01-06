a7e783a5c719f4bf4138f72e02eeb0a70c4ad670
koushik global <koushikofficial@yahoo.com>
2022-01-06 16:22:57 +0000

    Metrics and params added

--- src/split_data.py
+++ src/split_data.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from get_data import read_params
 
-def split_and_save_data(config_path):
-    config = read_params(config_path)
-    train_data_path=config["split_data"]["train_path"]
+def split_and_save_data(config_path): #it takes params.yaml as path 
+    config = read_params(config_path)  #params is the config path
+    train_data_path=config["split_data"]["train_path"] 
     test_data_path=config["split_data"]["test_path"]
     raw_data_path=config["load_data"]["raw_dataset_csv"]
     split_ratio = config["split_data"]["test_size"]
